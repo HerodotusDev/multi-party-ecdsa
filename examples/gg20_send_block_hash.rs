a51c42bd6d40d675c63512486e44f6368791fb53
tiagofneto <tiagofneto29@gmail.com>
2022-12-23 10:40:38 +0000

    initial flow

--- examples/gg20_send_block_hash.rs
+++ examples/gg20_send_block_hash.rs
@@ -1,10 +1,19 @@
-use anyhow::{Context, Result};
+use anyhow::{anyhow, Context, Result};
 use structopt::StructOpt;
 
 mod gg20_sm_client;
 use gg20_sm_client::join_computation;
 
+use std::path::PathBuf;
+
+use multi_party_ecdsa::protocols::multi_party_ecdsa::gg_2020::state_machine::sign::{
+    OfflineStage, SignManual,
+};
+use round_based::async_runtime::AsyncProtocol;
 use round_based::Msg;
+
+use curv::arithmetic::Converter;
+use curv::BigInt;
 
 use futures::{SinkExt, StreamExt, TryStreamExt};
 
@@ -14,7 +23,7 @@ use serde::{Serialize, Deserialize};
 //bytes32 - parenthash
 //uint256 - blocknumber
 //address - verifying contract address
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct BlockInfo {
     selector: String,
     parent_hash: String,
@@ -28,6 +37,10 @@ struct Cli {
     address: surf::Url,
     #[structopt(short, long, default_value = "block-hashes")]
     room: String,
+    #[structopt(short, long)]
+    local_share: PathBuf,
+    #[structopt(short, long, use_delimiter(true))]
+    parties: Vec<u16>,
 }
 
 #[tokio::main]
@@ -41,18 +54,68 @@ async fn main() -> Result<()> {
 
     tokio::pin!(outgoing);
 
+    let info = BlockInfo {
+        selector: "sel".to_string(),
+        parent_hash: "0x01".to_string(),
+        blocknumber: "420".to_string(),
+        address: "vitalikkkkk.eth".to_string(),
+    };
+
     outgoing
         .send(Msg {
             sender: i,
             receiver: None,
-            body: BlockInfo {
-                selector: "sel".to_string(),
-                parent_hash: "0x01".to_string(),
-                blocknumber: "420".to_string(),
-                address: "vitalikkkkk.eth".to_string(),
-            },
+            body: info.clone(),
         })
         .await?;
+
+    let local_share = tokio::fs::read(args.local_share)
+        .await
+        .context("cannot read local share")?;
+    let local_share = serde_json::from_slice(&local_share).context("parse local share")?;
+
+    let (i, incoming, outgoing) =
+        join_computation(args.address.clone(), &format!("{}-offline", args.room))
+            .await
+            .context("join offline computation")?;
+
+    let incoming = incoming.fuse();
+    tokio::pin!(incoming);
+    tokio::pin!(outgoing);
+
+    let number_of_parties = args.parties.len();
+
+    let signing = OfflineStage::new(i, args.parties, local_share)?;
+    let completed_offline_stage = AsyncProtocol::new(signing, incoming, outgoing)
+        .run()
+        .await
+        .map_err(|e| anyhow!("protocol execution terminated with error: {}", e))?;
+
+    let (i, incoming, outgoing) = join_computation(args.address, &format!("{}-online", args.room))
+        .await
+        .context("join online computation")?;
+
+    tokio::pin!(incoming);
+    tokio::pin!(outgoing);
+
+    let (signing, partial_signature) = SignManual::new(
+        BigInt::from_bytes(&bincode::serialize(&info).unwrap()),
+        completed_offline_stage,
+    )?;
+
+    let mut partial_signatures: Vec<_> = incoming
+        .take(number_of_parties-1)
+        .map_ok(|msg| msg.body)
+        .try_collect()
+        .await?;
+
+    partial_signatures.push(partial_signature);
+
+    let signature = signing
+        .complete(&partial_signatures)
+        .context("online stage failed")?;
+    let signature = serde_json::to_string(&signature).context("serialize signature")?;
+    println!("{}", signature);
 
     Ok(())
 }
--- examples/gg20_sign_block_hash.rs
+++ examples/gg20_sign_block_hash.rs
@@ -1,11 +1,22 @@
-use anyhow::{Context, Result};
+use anyhow::{anyhow, Context, Result};
 use structopt::StructOpt;
 
-use futures::{StreamExt, TryStreamExt};
+use futures::{SinkExt, StreamExt, TryStreamExt};
 use serde::{Serialize, Deserialize};
+
+use curv::arithmetic::Converter;
+use curv::BigInt;
+
+use std::path::PathBuf;
 
 mod gg20_sm_client;
 use gg20_sm_client::join_computation;
+
+use multi_party_ecdsa::protocols::multi_party_ecdsa::gg_2020::state_machine::sign::{
+    OfflineStage, SignManual,
+};
+use round_based::async_runtime::AsyncProtocol;
+use round_based::Msg;
 
 #[derive(StructOpt, Debug)]
 struct Cli {
@@ -13,6 +24,10 @@ struct Cli {
     address: surf::Url,
     #[structopt(short, long, default_value = "block-hashes")]
     room: String,
+    #[structopt(short, long)]
+    local_share: PathBuf,
+    #[structopt(short, long, use_delimiter(true))]
+    parties: Vec<u16>,
 }
 
 //bytes4 - method selector
@@ -37,18 +52,57 @@ async fn main() -> Result<()> {
             .await
             .context("join computation")?;
 
-    //let incoming = incoming.fuse();
+    let incoming = incoming.fuse();
     tokio::pin!(incoming);
     tokio::pin!(outgoing);
     
     // get the receiver, sign the hash and send the signature back to the receiver
-    let val: Vec<_> = incoming
+    let data_to_sign: Vec<_> = incoming
                 .take(1)
-                .map_ok(|msg| msg.body)
                 .try_collect()
                 .await?;
 
-    println!("{:?}", val);
+    let sender = data_to_sign[0].sender;
+
+    let local_share = tokio::fs::read(args.local_share)
+        .await
+        .context("cannot read local share")?;
+    let local_share = serde_json::from_slice(&local_share).context("parse local share")?;
+
+    let (i, incoming, outgoing) =
+        join_computation(args.address.clone(), &format!("{}-offline", args.room))
+            .await
+            .context("join offline computation")?;
+
+    let incoming = incoming.fuse();
+    tokio::pin!(incoming);
+    tokio::pin!(outgoing);
+
+    let signing = OfflineStage::new(i, args.parties, local_share)?;
+    let completed_offline_stage = AsyncProtocol::new(signing, incoming, outgoing)
+        .run()
+        .await
+        .map_err(|e| anyhow!("protocol execution terminated with error: {}", e))?;
+
+    let (i, incoming, outgoing) = join_computation(args.address, &format!("{}-online", args.room))
+        .await
+        .context("join online computation")?;
+
+    tokio::pin!(incoming);
+    tokio::pin!(outgoing);
+
+    let (signing, partial_signature) = SignManual::new(
+        BigInt::from_bytes(&bincode::serialize(&data_to_sign[0].body).unwrap()),
+        completed_offline_stage,
+    )?;
+
+    outgoing
+        .send(Msg {
+            sender: i,
+            receiver: Some(sender),
+            body: partial_signature,
+        })
+        .await?;
 
     Ok(())
 }
