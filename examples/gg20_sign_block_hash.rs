06fd35aa6b885fab1e00e3d1682b04cef206900d
tiagofneto <tiagofneto29@gmail.com>
2022-12-29 17:04:20 +0000

    stream block info to signers

--- examples/gg20_sign_block_hash.rs
+++ examples/gg20_sign_block_hash.rs
@@ -1,4 +1,6 @@
 use anyhow::{anyhow, Context, Result};
+use curv::elliptic::curves::Secp256k1;
+use multi_party_ecdsa::protocols::multi_party_ecdsa::gg_2020::state_machine::keygen::LocalKey;
 use structopt::StructOpt;
 
 use dotenv::dotenv;
@@ -37,90 +39,88 @@ async fn main() -> Result<()> {
     dotenv().ok();
     let args: Cli = Cli::from_args();
 
+    let local_share = tokio::fs::read(args.local_share)
+        .await
+        .context("cannot read local share")?;
+    let local_share: LocalKey<Secp256k1> = serde_json::from_slice(&local_share).context("parse local share")?;
+
     let (_i, incoming, _outgoing) =
         join_computation::<BlockInfo>(args.address.clone(), &args.room)
             .await
             .context("join computation")?;
 
-    let incoming = incoming.fuse();
     tokio::pin!(incoming);
-    
-    // get the receiver, sign the hash and send the signature back to the receiver
-    let data_to_sign: Vec<_> = incoming
-                .take(1)
-                .try_collect()
-                .await?;
-    println!("Received to sign: {:?}", data_to_sign);
 
-    let block_number = &data_to_sign[0].body.blocknumber;
-    let expected_hash = &data_to_sign[0].body.parent_hash;
+    // fetch all the blocks info
+    while let Some(block_info) = incoming.next().await {
+        let data_to_sign = block_info.unwrap();
+        println!("Received to sign: {:?}", data_to_sign);
 
-    let client = reqwest::Client::new();
-    let rpc = std::env::var("ETHEREUM_RPC").unwrap();
+        let block_number = &data_to_sign.body.blocknumber;
+        let expected_hash = &data_to_sign.body.parent_hash;
 
-    let res = client.post(&rpc)
-        .body(format!(r#"{{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["{}", true],"id":1}}"#, block_number))
-        .send()?
-        .text()?;
+        let client = reqwest::Client::new();
+        let rpc = std::env::var("ETHEREUM_RPC").unwrap();
 
-    let re = Regex::new(r#""parentHash":"([^"]+)"#).unwrap();
-    let captures = re.captures(&res).unwrap();
-    let hash = &captures[1];
+        let res = client.post(&rpc)
+            .body(format!(r#"{{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["{}", true],"id":1}}"#, block_number))
+            .send()?
+            .text()?;
 
-    // Verifying hash
-    assert_eq!(expected_hash, hash, "Invalid hash");
+        let re = Regex::new(r#""parentHash":"([^"]+)"#).unwrap();
+        let captures = re.captures(&res).unwrap();
+        let hash = &captures[1];
 
-    //let sender = data_to_sign[0].sender;
+        // Verifying hash
+        assert_eq!(expected_hash, hash, "Invalid hash");
 
-    let local_share = tokio::fs::read(args.local_share)
-        .await
-        .context("cannot read local share")?;
-    let local_share = serde_json::from_slice(&local_share).context("parse local share")?;
+        //let sender = data_to_sign.sender;
+        
+        let (i, incoming, outgoing) =
+            join_computation(args.address.clone(), &format!("{}-offline", args.room))
+                .await
+                .context("join offline computation")?;
 
-    let (i, incoming, outgoing) =
-        join_computation(args.address.clone(), &format!("{}-offline", args.room))
+        let incoming = incoming.fuse();
+        tokio::pin!(incoming);
+        tokio::pin!(outgoing);
+
+        println!("1------------------ Before offline ");
+
+        let signing = OfflineStage::new(i, args.parties.clone(), local_share.clone())?;
+        let completed_offline_stage = AsyncProtocol::new(signing, incoming, outgoing)
+            .run()
             .await
-            .context("join offline computation")?;
+            .map_err(|e| anyhow!("protocol execution terminated with error: {}", e))?;
 
-    let incoming = incoming.fuse();
-    tokio::pin!(incoming);
-    tokio::pin!(outgoing);
+        println!("2------------------ Offline completed ");
 
-    println!("1------------------");
+        let (i, _incoming, outgoing) = join_computation(args.address.clone(), &format!("{}-online", args.room))
+            .await
+            .context("join online computation")?;
 
-    let signing = OfflineStage::new(i, args.parties, local_share)?;
-    let completed_offline_stage = AsyncProtocol::new(signing, incoming, outgoing)
-        .run()
-        .await
-        .map_err(|e| anyhow!("protocol execution terminated with error: {}", e))?;
+        tokio::pin!(outgoing);
 
-    println!("2------------------");
+        let (_signing, partial_signature) = SignManual::new(
+            BigInt::from_bytes(&bincode::serialize(&data_to_sign.body).unwrap()),
+            completed_offline_stage,
+        )?;
 
-    let (i, _incoming, outgoing) = join_computation(args.address, &format!("{}-online", args.room))
-        .await
-        .context("join online computation")?;
+        println!("3------------------ Partial signature completed, sending to master node");
 
-    tokio::pin!(outgoing);
+        outgoing
+            .send(Msg {
+                sender: i,
+                // TODO: receiver to master node
+                //receiver: Some(sender),
+                receiver: None,
+                body: partial_signature.clone(),
+            })
+            .await?;
 
-    let (_signing, partial_signature) = SignManual::new(
-        BigInt::from_bytes(&bincode::serialize(&data_to_sign[0].body).unwrap()),
-        completed_offline_stage,
-    )?;
-
-    println!("3------------------");
-
-    outgoing
-        .send(Msg {
-            sender: i,
-            // TODO: receiver to master node
-            //receiver: Some(sender),
-            receiver: None,
-            body: partial_signature.clone(),
-        })
-        .await?;
-
-    //println!("{:?} sent partial_signature {:?} to {:?}", i, partial_signature, sender);
-    println!("{:?} sent partial_signature {:?}", i, partial_signature);
-
+        //println!("{:?} sent partial_signature {:?} to {:?}", i, partial_signature, sender);
+        println!("{:?} sent partial_signature {:?}", i, partial_signature);
+    }
+    
     Ok(())
 }
